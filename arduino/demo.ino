e909fd0d16d3057ef15044c8c751384e93b2d5b5
keetsm1 <145614621+keetsm1@users.noreply.github.com>
2025-03-28 15:17:06 -0400

    Update demo.ino

--- arduino/demo.ino
+++ arduino/demo.ino
@@ -9,6 +9,9 @@ const int flexSensorPin3 = 34; //middle finger
 const int flexSensorPin4 = 35; //ring finger
 const int flexSensorPin5 = 32; //small finger
 
+// Calibration values
+const int minValues[5] = {0, 0, 0, 0, 0};     
+const int maxValues[5] = {675, 800, 750, 120, 430};
 
 void setup() {
   Serial.begin(115200);           // Start serial communication for debugging
@@ -20,37 +23,36 @@ void setup() {
   pinMode(flexSensorPin3, INPUT);
   pinMode(flexSensorPin4, INPUT);
   pinMode(flexSensorPin5, INPUT);
-
 }
 
-int flexMap(int sensorValue){
-  int angle = map(sensorValue, 0, 1920, 0, 180);
+// Precise mapping function that inverts the value if needed
+int flexMapPrecise(int sensorValue, int minVal, int maxVal) {
+  // Constrain the value to prevent out-of-range mapping
+  int constrainedValue = constrain(sensorValue, minVal, maxVal);
+  
+  // Invert the mapping so that low values = 0, high values = 180
+  // This assumes the sensor reads low when fully extended (finger down)
+  // and high when fully bent (finger up)
+  int angle = map(constrainedValue, minVal, maxVal, 0, 180);
+  
   return angle;
 }
 
 void loop() {
-  // Read the sensor value
+  // Read the sensor values
   int sensorValue1 = analogRead(flexSensorPin1);
   int sensorValue2 = analogRead(flexSensorPin2);
   int sensorValue3 = analogRead(flexSensorPin3);
   int sensorValue4 = analogRead(flexSensorPin4);
-  // int sensorValue5 = analogRead(flexSensorPin5);
+  int sensorValue5 = analogRead(flexSensorPin5);
 
-  // int sensorValue4 = 1900;
-  int sensorValue5 = 1900;
-
-  
-
-    
-   
-
-  // Define the other values (can be fixed or dynamically computed)
-  
-  // Debug output to Serial Monitor
-  
-  // Construct a string with all 5 values, separated by commas
-  String data = String((sensorValue1)) + "," + String((sensorValue2)) + "," + String((sensorValue3)) 
-                    + "," + String((sensorValue4)) + "," + String((sensorValue5)) ;
+  // Create data string with precise mapping
+  String data = 
+    String(flexMapPrecise(sensorValue1, 0, maxValues[0])) + "," + 
+    String(flexMapPrecise(sensorValue2, 0, maxValues[1])) + "," + 
+    String(flexMapPrecise(sensorValue3, 0, maxValues[2])) + "," + 
+    String(flexMapPrecise(sensorValue4, 0, maxValues[3])) + "," + 
+    String(flexMapPrecise(sensorValue5, 0, maxValues[4]));
 
   // Send the constructed string over Bluetooth
   SerialBT.println(data);
@@ -59,7 +61,4 @@ void loop() {
   Serial.println("Sent Data: " + data);
 
   delay(100); // Delay to limit update rate
- 
 }
-
-
